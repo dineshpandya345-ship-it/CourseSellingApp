73c0cb4f60abe6e8f5387c01a85a183cf8380223
Dhruv Pandya <dineshpandya345@gmail.com>
2026-06-02 10:43:24 +0530

    admin signup password hashed done

--- routes/admin.js
+++ routes/admin.js
@@ -2,31 +2,37 @@ const express = require("express");
 const adminRoutes = express();
 const { AdminModel } = require("../db.js");
 adminRoutes.use(express.json());
+const bcrypt = require("bcrypt");
+
 
 adminRoutes.post("/signup", async (req, res) => {
     const { email, password, firstName, lastName } = req.body;
     try {
+        const hashedPassword = await bcrypt.hash(password, 1);
         const response = await AdminModel.create({
             email: email,
-            password: password,
+            password: hashedPassword,
             firstName: firstName,
             lastName: lastName
         })
         res.status(200).json({
-            mssge: "user signed up successfully!!"
+            mssge: "signed up successfull!!"
         })
     }
     catch (err) {
         console.log(err);
         res.json({
-            mssge: "user is not signed up!!!"
+            mssge: "User name is taken!!!!"
         })
     }
 
 })
 adminRoutes.post(".signin", (req, res) => {
     const { email, password } = req.body;
+
 })
 module.exports = {
     adminRoutes: adminRoutes
 }
+
+
